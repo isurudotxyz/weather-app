e0ea775ce2f5cf8c1aa4be91adbbd47de6dd52a8
isurudotxyz <agisuru@icloud.com>
2023-09-06 14:09:52 +0200

    search city function

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const apiKey = "7b8cbf4c36a54d1d4b4b69a90dc5158a";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=italy";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   let data = await response.json();
 
   console.log(data);
 
   document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  console.log(data.main.humidity);
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
